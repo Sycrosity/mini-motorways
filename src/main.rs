29df8694ed15808c80d8536d0888b8d0471a00e4
Sycrosity <72102935+Sycrosity@users.noreply.github.com>
2025-06-15 18:11:26 +0100

    feat: simple bevy app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest())) // prevents blurry sprites
+        .add_systems(Startup, setup)
+        // .add_systems(Update, animate_sprite)
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
+) {
+    let texture = asset_server.load("textures/sprite_sheet.png");
+    let layout = TextureAtlasLayout::from_grid(UVec2::splat(32), 10, 10, None, None);
+    let texture_atlas_layout = texture_atlas_layouts.add(layout);
+    // Use only the subset of sprites in the sheet that make up the run animation
+    // let animation_indices = AnimationIndices { first: 1, last: 6 };
+
+    commands.spawn(Camera2d);
+
+    commands.spawn((
+        Sprite::from_atlas_image(
+            texture,
+            TextureAtlas {
+                layout: texture_atlas_layout,
+                index: 0,
+            },
+        ),
+        Transform::from_scale(Vec3::splat(6.0)),
+        // animation_indices,
+        // AnimationTimer(Timer::from_seconds(0.1, TimerMode::Repeating)),
+    ));
 }
